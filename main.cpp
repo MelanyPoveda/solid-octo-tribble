4b5f9d2b67d4cfef8f5d844a0726e38c13d3ca8a
MelanyPoveda <melanypovedawilliams@gmail.com>
2026-02-21 13:53:14 -0600

    basic c++

---
+++ main.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+int main (int argc, char *argv[]) {
+	string nombre;
+	int edad;
+	string nacimiento;
+	
+	cout << "Como te llamas\n"; // \n = enter same as python and you also can use endl
+	cin >> nombre;
+	
+	cout << "Cual es tu edad\n";
+	cin >> edad;
+	
+	cout << "En qu" << char(130) << " a" << char(164) << "o naciste\n"; //special characters = char
+	cin >> nacimiento;
+		
+	cout << "\n\n\n\nY se quien eres, eres " << nombre << " y tienenes " << edad << " a" << char(164) << "os";
+	return 0;
+}
+
