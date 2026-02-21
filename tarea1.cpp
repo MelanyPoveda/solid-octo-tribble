14560fe6bad202352ea0a8cee259c6cc9a18b5cc
MelanyPoveda <melanypovedawilliams@gmail.com>
2026-02-21 14:30:34 -0600

    basic c++

---
+++ tarea1.cpp
@@ -0,0 +1,42 @@
+#include<iostream>
+using namespace std;
+
+int main (int argc, char *argv[]) {
+	
+	cout << "********************\n";
+	cout << "*                  *\n";
+	cout << "*   Bienvenido!    *\n";
+	cout << "*                  *\n";
+	cout << "********************\n";
+	
+	cout << "======================\n";
+	cout << "||                  ||\n";
+	cout << "||   Bienvenido!    ||\n";
+	cout << "||                  ||\n";
+	cout << "======================\n";
+	
+	cout << "+----------+\n";
+	cout << "|          |\n";
+	cout << "|   Hola!  |\n";
+	cout << "|          |\n";
+	cout << "+----------+\n";
+	
+	cout << "      * \n";
+	cout << "    * * \n";
+	cout << "  * * * \n";
+	cout << "* * * * \n\n";
+	
+	cout << "* * * * \n";
+	cout << "* * *   \n";
+	cout << "* *     \n";
+	cout << "*       \n\n";
+	
+	cout << "* * * * \n";
+	cout << "* * * * \n";
+	cout << "* * * * \n";
+	cout << "* * * * \n";
+	
+	
+	return 0;
+}
+
